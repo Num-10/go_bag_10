d3ef4e1e5325268a57c2e2dd391ac4d1994f2683
num_10 fc <num_10@163.com>
2020-01-02 17:16:27 +0800

    edit model field

--- model/article.go
+++ model/article.go
@@ -1,9 +1,14 @@
 package model
 
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
 type Article struct {
 	Content       string `gorm:"column:content"`
 	CoverImageURL string `gorm:"column:cover_image_url"`
-	CreatedAt     int    `gorm:"column:created_at"`
+	Created       int    `gorm:"column:created"`
 	Desc          string `gorm:"column:desc"`
 	ID            int    `gorm:"column:id;primary_key"`
 	IsMarrow      int    `gorm:"column:is_marrow"`
@@ -12,12 +17,22 @@ type Article struct {
 	Status        int    `gorm:"column:status"`
 	TagID         int    `gorm:"column:tag_id"`
 	Title         string `gorm:"column:title"`
-	UpdatedAt     int    `gorm:"column:updated_at"`
+	Updated       int    `gorm:"column:updated"`
 	UserID        int    `gorm:"column:user_id"`
 }
 
 // TableName sets the insert table name for this struct type
 func (a *Article) TableName() string {
 	return "blog_article"
+}
+
+func (a *Article) BeforeCreate(scope *gorm.Scope) error {
+	scope.SetColumn("Created", time.Now().Unix())
+	return nil
+}
+
+func (a *Article) BeforeUpdate(scope *gorm.Scope) error {
+	scope.SetColumn("Updated", time.Now().Unix())
+	return nil
 }
 
